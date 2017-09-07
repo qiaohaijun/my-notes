7ee00280d62f2b6679c029e9c83f41ea148c9446
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2017-09-07 10:52:24 +0800

    Create mutable.cpp

---
+++ cpp/case_by_case/mutable/mutable.cpp
@@ -0,0 +1,17 @@
+// mutable.cpp  
+class X  
+{  
+public:  
+   bool GetFlag() const  
+   {  
+      m_accessCount++;  
+      return m_flag;  
+   }  
+private:  
+   bool m_flag;  
+   mutable int m_accessCount;  
+};  
+
+int main()  
+{  
+}  
