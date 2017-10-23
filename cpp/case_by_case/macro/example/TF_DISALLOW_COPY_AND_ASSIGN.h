444e353b766cff1e0a62c1f3ddfe476d4254243a
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2017-10-23 16:52:48 +0800

    Create TF_DISALLOW_COPY_AND_ASSIGN.h

---
+++ cpp/case_by_case/macro/example/TF_DISALLOW_COPY_AND_ASSIGN.h
@@ -0,0 +1,5 @@
+// A macro to disallow the copy constructor and operator= functions
+// This is usually placed in the private: declarations for a class.
+#define TF_DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&) = delete;         \
+  void operator=(const TypeName&) = delete
