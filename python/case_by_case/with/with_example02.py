e57c2758e874f05d49a0fb280e500fb4398331be
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2017-10-31 12:53:16 +0800

    Create with_example02.py

---
+++ python/case_by_case/with/with_example02.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# with_example02.py
+
+
+class Sample:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, trace):
+        print "type:", type
+        print "value:", value
+        print "trace:", trace
+        
+    def do_something(self):
+        bar = 1/0
+        return bar + 10
+
+with Sample() as sample:
+    sample.do_something()
