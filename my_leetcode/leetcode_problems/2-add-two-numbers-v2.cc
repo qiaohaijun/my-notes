f586dd1c62b80a810ba96ad1838686d573d2023b
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-04-10 23:55:40 +0800

    Create 2-add-two-numbers-v2.cc

---
+++ my_leetcode/leetcode_problems/2-add-two-numbers-v2.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *res = new ListNode(-1);
+        ListNode *cur = res;
+        int carry = 0;
+        while (l1 || l2) {
+            int n1 = l1 ? l1->val : 0;
+            int n2 = l2 ? l2->val : 0;
+            int sum = n1 + n2 + carry;
+            carry = sum / 10;
+            cur->next = new ListNode(sum % 10);
+            cur = cur->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        if (carry) cur->next = new ListNode(1);
+        return res->next;
+    }
+};
