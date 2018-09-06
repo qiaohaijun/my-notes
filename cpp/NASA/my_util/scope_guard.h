03b413ef190f561afcd5ae42df48c8e5bf6923de
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-09-06 16:44:36 +0800

    Create scope_guard.h

---
+++ cpp/NASA/my_util/scope_guard.h
@@ -0,0 +1,42 @@
+template <typename F>
+class ScopeGuard
+{
+public:
+    explicit ScopeGuard( F && f) : m_func(std::move(f)), m_dismiss(false){}
+    explicit ScopeGuard(const F& f) : m_func(f), m_dismiss(false){}
+
+    ~ScopeGuard()
+    {
+        if (!m_dismiss)
+            m_func();
+    }
+
+    ScopeGuard(ScopeGuard && rhs) : m_func(std::move(rhs.m_func)), m_dismiss(rhs.m_dismiss){rhs.Dismiss();}
+
+    void Dismiss()
+    {
+        m_dismiss = true;
+    }
+
+private:
+    F m_func;
+    bool m_dismiss;
+
+    ScopeGuard();
+    ScopeGuard(const ScopeGuard&);
+    ScopeGuard& operator=(const ScopeGuard&);
+
+
+//template<class... Args>
+//auto Run(Args&&... args)->typename std::result_of<F(Args...)>::type
+//{
+//　　return m_func(std::forward<Args>(args)...);
+//}
+};
+
+
+template <typename F>
+ScopeGuard<typename std::decay<F>::type> MakeGuard(F && f)
+{
+	　　return ScopeGuard<typename std::decay<F>::type>(std::forward<F>(f));
+}
