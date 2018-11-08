d697cafd24f491f5251e3f67d280dec6be4893b6
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-11-08 18:45:12 +0800

    Create handler_guard.h

---
+++ cpp/my_project/util/guard/handler_guard.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <functional>
+
+namespace simcc {
+
+class HandlerGuard {
+public:
+    typedef std::function<void()> Handler;
+
+public:
+    HandlerGuard(Handler h)
+        : cb_(h), execute_(true) {}
+
+    ~HandlerGuard() {
+        if (execute_) {
+            cb_();
+        }
+    }
+
+    void set_execute(bool v) { execute_ = v; }
+private:
+    Handler cb_;
+    bool execute_;
+};
+
+}
