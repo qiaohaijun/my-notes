853ff9fa0cef435835f5dc23be7e5d60c574adaa
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-12-29 16:53:34 +0800

    Create trans_svn_tags_to_git_format.sh

---
+++ git/git-svn/trans_svn_tags_to_git_format.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+# svn tag => git tag
+git for-each-ref refs/remotes/tags | cut -d / -f 4- | grep -v @ | while read tagname; do git tag "$tagname" "tags/$tagname"; git branch -r -d "tags/$tagname"; done
