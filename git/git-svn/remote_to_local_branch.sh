cbb5fae743b4a0740c16890bca01c26312e87369
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-12-29 16:55:40 +0800

    Create remote_to_local_branch.sh

---
+++ git/git-svn/remote_to_local_branch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+
+git for-each-ref refs/remotes | cut -d / -f 3- | grep -v @ | while read branchname; do git branch "$branchname" "refs/remotes/$branchname"; git branch -r -d "$branchname"; done
